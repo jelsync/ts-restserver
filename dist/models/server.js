9553038037fc10c8d8d671112f353a5d6ea72b2f
jelsyn <jelsync@gmail.com>
2021-05-19 22:38:51 -0600

    Agregando rutas al server

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,14 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const usuario_1 = __importDefault(require("../routes/usuario"));
 class Server {
     constructor() {
+        this.apiPaths = {
+            usuarios: '/api/usuarios'
+        };
         this.app = express_1.default();
         this.port = process.env.PORT || '8000';
+        //definir rutas
+        this.routes();
+    }
+    routes() {
+        this.app.use(this.apiPaths.usuarios, usuario_1.default);
     }
     listen() {
         this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto' + this.port);
+            console.log('Servidor corriendo en puerto ' + this.port);
         });
     }
 }
--- models/server.ts
+++ models/server.ts
@@ -1,18 +1,28 @@
 import express, { Application } from "express";
+import userRoutes from "../routes/usuario";
 
 class Server {
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+        //definir rutas
+        this.routes();
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRoutes);
     }
 
     listen() {
         this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto' + this.port);
+            console.log('Servidor corriendo en puerto ' + this.port);
         })
     }
 }
