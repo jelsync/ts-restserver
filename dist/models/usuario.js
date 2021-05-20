f96878fe3514e400070e5b065dab3ef6793ac474
jelsyn <jelsync@gmail.com>
2021-05-20 13:00:02 -0600

    Creacion modelo usuario

---
+++ dist/models/usuario.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const connection_1 = __importDefault(require("../db/connection"));
+const usuario = connection_1.default.define('Usuario', {
+    nombre: {
+        type: sequelize_1.DataTypes.STRING
+    },
+    email: {
+        type: sequelize_1.DataTypes.STRING
+    },
+    estado: {
+        type: sequelize_1.DataTypes.BOOLEAN
+    },
+});
+exports.default = usuario;
+//# sourceMappingURL=usuario.js.map
