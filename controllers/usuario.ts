3d5448cb09e2320f08bbafc1f7886c335d710d49
jelsyn <jelsync@gmail.com>
2021-05-20 16:00:50 -0600

    Crud usuario

--- controllers/usuario.ts
+++ controllers/usuario.ts
@@ -1,45 +1,95 @@
 import { Request, Response } from "express";
+import Usuario from "../models/usuario";
 
-export const getUsuarios = (req: Request, res: Response) => {
-    res.json({
-        mgs: 'Usuarios'
-    });
+export const getUsuarios = async (req: Request, res: Response) => {
+    const usuarios = await Usuario.findAll();
+    res.json(usuarios);
 }
 
-export const getUsuario = (req: Request, res: Response) => {
+export const getUsuario = async (req: Request, res: Response) => {
     const { id } = req.params;
+    const usuario = await Usuario.findByPk(id);
 
-    res.json({
-        mgs: 'Usuarios',
-        id
-    });
+    if (usuario) {
+        res.json(usuario);
+    } else {
+        res.json({
+            mgs: 'Usuario no encontrado'
+        })
+    }
+
 }
 
-export const postUsuario = (req: Request, res: Response) => {
+export const postUsuario = async (req: Request, res: Response) => {
     const { body } = req;
 
-    res.json({
-        mgs: 'postUsuario',
-        body
-    });
+    try {
+        const existeEmial = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        });
+        if (existeEmial) {
+            return res.json({
+                mgs: 'Ya existe corre ' + body.email
+            });
+        }
+        const usuario = Usuario.build(body);
+        // const usuario = new Usuario(body);
+        await usuario.save();
+        res.json(usuario);
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            mgs: 'Hable con el administrado',
+        });
+
+    }
+
 }
 
-export const putUsuario = (req: Request, res: Response) => {
+export const putUsuario = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { body } = req;
 
-    res.json({
-        mgs: 'putUsuario',
-        body,
-        id
-    });
+    try {
+        const usuario = await Usuario.findByPk(id);
+
+        if (!usuario) {
+            return res.json({
+                mgs: 'No existe usuario ' + body.nombre
+            });
+        }
+
+        await usuario.update(body);
+
+        res.json(usuario);
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            mgs: 'Hable con el administrado',
+        });
+
+    }
 }
 
-export const deleteUsuario = (req: Request, res: Response) => {
+export const deleteUsuario = async (req: Request, res: Response) => {
     const { id } = req.params;
 
+    const usuario = await Usuario.findByPk(id);
+
+    if (!usuario) {
+        return res.json({
+            mgs: 'No existe usuario '
+        });
+    }
+
+    // await usuario.destroy(); ELIMINAR USUARIO DE LA BASE DE DATOS
+    await usuario.update({ estado: false });
+
     res.json({
-        mgs: 'deleteUsuarios',
-        id
+        mgs: 'Usuario eliminado'
     });
 }
--- dist/controllers/usuario.js
+++ dist/controllers/usuario.js
@@ -1,44 +1,97 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUsuario = exports.putUsuario = exports.postUsuario = exports.getUsuario = exports.getUsuarios = void 0;
-const getUsuarios = (req, res) => {
-    res.json({
-        mgs: 'Usuarios'
-    });
-};
+const usuario_1 = __importDefault(require("../models/usuario"));
+const getUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const usuarios = yield usuario_1.default.findAll();
+    res.json(usuarios);
+});
 exports.getUsuarios = getUsuarios;
-const getUsuario = (req, res) => {
+const getUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        mgs: 'Usuarios',
-        id
-    });
-};
+    const usuario = yield usuario_1.default.findByPk(id);
+    if (usuario) {
+        res.json(usuario);
+    }
+    else {
+        res.json({
+            mgs: 'Usuario no encontrado'
+        });
+    }
+});
 exports.getUsuario = getUsuario;
-const postUsuario = (req, res) => {
+const postUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        mgs: 'postUsuario',
-        body
-    });
-};
+    try {
+        const existeEmial = yield usuario_1.default.findOne({
+            where: {
+                email: body.email
+            }
+        });
+        if (existeEmial) {
+            return res.json({
+                mgs: 'Ya existe corre ' + body.email
+            });
+        }
+        const usuario = usuario_1.default.build(body);
+        // const usuario = new Usuario(body);
+        yield usuario.save();
+        res.json(usuario);
+    }
+    catch (error) {
+        console.log(error);
+        res.json({
+            mgs: 'Hable con el administrado',
+        });
+    }
+});
 exports.postUsuario = postUsuario;
-const putUsuario = (req, res) => {
+const putUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        mgs: 'putUsuario',
-        body,
-        id
-    });
-};
+    try {
+        const usuario = yield usuario_1.default.findByPk(id);
+        if (!usuario) {
+            return res.json({
+                mgs: 'No existe usuario ' + body.nombre
+            });
+        }
+        yield usuario.update(body);
+        res.json(usuario);
+    }
+    catch (error) {
+        console.log(error);
+        res.json({
+            mgs: 'Hable con el administrado',
+        });
+    }
+});
 exports.putUsuario = putUsuario;
-const deleteUsuario = (req, res) => {
+const deleteUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
+    const usuario = yield usuario_1.default.findByPk(id);
+    if (!usuario) {
+        return res.json({
+            mgs: 'No existe usuario '
+        });
+    }
+    // await usuario.destroy(); ELIMINAR USUARIO DE LA BASE DE DATOS
+    yield usuario.update({ estado: false });
     res.json({
-        mgs: 'deleteUsuarios',
-        id
+        mgs: 'Usuario eliminado'
     });
-};
+});
 exports.deleteUsuario = deleteUsuario;
 //# sourceMappingURL=usuario.js.map
